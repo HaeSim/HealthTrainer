a1632310eb6f94673969a83e9bbe0dbfb97c7992
HaeSim <anjin309@gmail.com>
2016-11-06 02:20:02 +0900

    첫번째 commit

---
+++ app/src/main/java/info/androidhive/HealthTrainer/activity/SplashActivity.java
@@ -0,0 +1,40 @@
+package info.androidhive.HealthTrainer.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
+
+import info.androidhive.HealthTrainer.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, 1200);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
